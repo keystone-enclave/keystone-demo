56075248f9fa51e565dd65f71d9f6adeef301fdd
ThaumicMekanism <thaumicmekanism@gmail.com>
2019-03-04 18:19:01 -0800

    Generalized the source

--- source.sh
+++ source.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export LIBSODIUM_CLIENT_DIR=~/keystone/keystone-demo/libsodium_builds/libsodium_client/src/libsodium
-export LIBSODIUM_DIR=~/keystone/keystone-demo/libsodium_builds/libsodium_server/src/libsodium
-export KEYSTONE_SDK_DIR=~/keystone/sdk
+export LIBSODIUM_CLIENT_DIR=$(pwd)/libsodium_builds/libsodium_client/src/libsodium
+export LIBSODIUM_DIR=$(pwd)/libsodium_builds/libsodium_server/src/libsodium
+# export KEYSTONE_SDK_DIR=~/keystone/sdk
