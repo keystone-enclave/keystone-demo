603f15dcec40964de3d43aa3fe0cf9a70efc2d4b
ThaumicMekanism <thaumicmekanism@gmail.com>
2019-03-04 18:23:35 -0800

    removed comment

--- source.sh
+++ source.sh
@@ -2,4 +2,3 @@
 
 export LIBSODIUM_CLIENT_DIR=$(pwd)/libsodium_builds/libsodium_client/src/libsodium
 export LIBSODIUM_DIR=$(pwd)/libsodium_builds/libsodium_server/src/libsodium
-# export KEYSTONE_SDK_DIR=~/keystone/sdk
